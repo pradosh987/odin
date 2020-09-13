f6f926842b57ac82c9530bd82912dd89237bae62
Pradosh Gaonkar <pradosh987@gmail.com>
2020-09-13 21:32:00 +0530

    feat: themepack scrapper

---
+++ src/scrappers/themepack_scrapper.ts
@@ -0,0 +1,38 @@
+import { BaseScrapper } from "./base_scrapper";
+import { logger } from "../core/logger";
+import path from "path";
+
+export class ThemepackScrapper extends BaseScrapper {
+  featuredImage(): string | undefined {
+    return undefined;
+  }
+
+  images(): string[] {
+    return <string[]>this.document("#image-gallery img")
+      .toArray()
+      .map(this.getSrcFromImgTag)
+      .map((i) => path.join(this.url.origin, <string>i));
+  }
+
+  isTheme(): boolean {
+    return this.url.pathname.includes("/theme/");
+  }
+
+  themeName(): string {
+    return this.h1();
+  }
+
+  wallpapersCount(): number | undefined {
+    try {
+      return this.document(".uk-list li")
+        .last()
+        .text()
+        .split(" ")
+        .map(Number)
+        .filter(Boolean)[0];
+    } catch (e) {
+      logger.warn(e, this.url);
+      return undefined;
+    }
+  }
+}
