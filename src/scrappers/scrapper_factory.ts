f40c66177fce8991fbc96dfbcabf2d2e5815bd2f
Pradosh Gaonkar <pradosh987@gmail.com>
2020-09-16 16:11:30 +0530

    fix: ignore ssl error for themepack

--- src/scrappers/scrapper_factory.ts
+++ src/scrappers/scrapper_factory.ts
@@ -1,23 +1,31 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import cheerio from "cheerio";
 import { ExpoThemesScrapper } from "./expothemes_scrapper";
 import { MicrosoftThemeStoreScrapper } from "./microsoft_theme_store_scrapper";
 import { ThemeRaiderScrapper } from "./theme_raider_scrapper";
 import { ThemepackScrapper } from "./themepack_scrapper";
+import https from "https";
 
 export const buildScrapper = async (url: URL) => {
-  const rawHtml = (await axios.get(url.toString())).data;
-
   switch (url.host) {
     case "www.expothemes.com":
-      return new ExpoThemesScrapper(url, rawHtml);
-    case "www.microsoft.com":
-      return new MicrosoftThemeStoreScrapper(url, rawHtml);
+      return new ExpoThemesScrapper(url, await get(url.toString()));
     case "www.themeraider.com":
-      return new ThemeRaiderScrapper(url, rawHtml);
+      return new ThemeRaiderScrapper(url, await get(url.toString()));
+    case "www.microsoft.com":
+      return new MicrosoftThemeStoreScrapper(url, await get(url.toString()));
     case "themepack.me":
-      return new ThemepackScrapper(url, rawHtml);
+      const agent = new https.Agent({
+        rejectUnauthorized: false,
+      });
+      return new ThemepackScrapper(
+        url,
+        await get(url.toString(), { httpsAgent: agent })
+      );
     default:
-      throw Error(`No scrapper found for ${url}`);
+      throw Error(`No scrapper found for ${url.host}, ${url.hostname}`);
   }
 };
+
+const get = (url: string, config: AxiosRequestConfig = {}) =>
+  axios.get(url, config).then((res) => res.data);
